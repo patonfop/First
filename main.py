6b94803ccf7818ba38ed5fe96bbfdc90dc61767e
Kulik Roman <romankylik@gmail.com>
2023-03-03 14:02:08 +0200

    Витягнув повну назву країни, коротку та картинки

--- main.py
+++ main.py
@@ -6,15 +6,32 @@ def country ():
     r = requests.get(URL)
     if r.status_code == 200:
         soup = b(r.text, 'html.parser')
-        all_cauntr = soup.find('table', class_='wikitable').find_all('img', class_='thumbborder')
+        cauntr_list = []
+        img_list = []
+        full_name = []
+        all_cauntr = soup.find('table', class_='wikitable').find_all('tr')
+        for i in all_cauntr[1:]:
+            cauntr_list.append(i.find_all("td")[1].find('img', class_='thumbborder').get('alt'))
+            img_list.append(i.find_all("td")[1].find('img', class_='thumbborder').get('src'))
+            full_name.append(i.find_all("td")[3].text)
+
+
+
+        print(full_name)
+        print(cauntr_list)
+        print(img_list)
+
+
+
+        """all_cauntr = soup.find('table', class_='wikitable').find_all('img', class_='thumbborder')
         cauntr_list = []
         img_list = []
         for i in all_cauntr:
             cauntr_list.append(i.get('alt'))
-            img_list.append(i.get('src'))
+            img_list.append(i.get('src'))"""
 
-        print(cauntr_list)
-        print(img_list)
+        #print(cauntr_list)
+
 """        
         img = soup.find('img', class_='weatherImg').get('title')
         return {temp_min.text, temp_max.text, img}
