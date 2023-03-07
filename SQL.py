c5985bd29da957fcecbe60ca3914f9211d3872cc
Kulik Roman <romankylik@gmail.com>
2023-03-07 17:14:49 +0200

    Створив таблицю "країни"

--- SQL.py
+++ SQL.py
@@ -10,6 +10,27 @@ def create_connection(path):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-    return connection
-
+    return connection  #Створили (Підключились) до БД:
 connection = create_connection("./country_DB")
+
+def execute_query(connection, query): #виконати задане query
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+create_countries_table = """
+CREATE TABLE IF NOT EXISTS countries (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  full_country_name TEXT,
+  words_in_full_name INTEGER,
+  same_letter_count INTEGER,
+  flag_url TEXT
+);
+"""
+
+execute_query(connection, create_countries_table)
